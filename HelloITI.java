747471d9904b6d2164199dcde75e6b96cbfee8f4
Ziad-Elshemy <zizoelshemy@gmail.com>
2025-01-09 06:00:08 +0200

    add printHelloGit Function in HelloITI class

--- HelloITI.java
+++ HelloITI.java
@@ -1,11 +1,15 @@
 class HelloITI {
     public static void main(String[] args) {
         printHelloITI();
+		printHelloGit();
     }
 	
 	public static void printHelloITI(){
 		System.out.println("Hello ITI");
 	}
 	
+	public static void printHelloGit(){
+		System.out.println("Hello Git");
+	}
 } 
  
